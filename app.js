a40982b0039636fbe822874f27079aa978fef41d
Blake Heyman <blake.heyman@gmail.com>
2023-04-09 22:06:57 -0400

    working local

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ import HelloController from "./controllers/hello-controller.js";
 import UserController from "./controllers/users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 import cors from "cors";
+import mongoose from "mongoose";
+import * as dotenv from "dotenv";
+dotenv.config();
+const CONNECTION_STRING =
+  process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/tuiter";
+console.log(CONNECTION_STRING);
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(cors());
 app.use(express.json());
